871f7d80337aa064ae6374e7e432d5574d494e6b
raphael mendes <raphael_smendes@hotmail.com>
2021-12-13 16:13:38 -0300

    Primeira adicao de arquivos

---
+++ README.md
@@ -0,0 +1,2 @@
+Atividade referente a unidade UC8 - Encontro Remoto 2, onde foi feito um algoritmo apresentando alguns operadores de 
+condição.
---
+++ alunos.js
@@ -0,0 +1,37 @@
+let nomeAlunos =["Marcelo", "Joao", "Danilo", "Roger","Jessica","Isabela"]
+
+for (let nome of nomeAlunos) {
+    console.log(`Esta pessoa se chama ${nome}`)    
+}
+
+
+
+
+// let numeroDeAlunos = 10;
+
+// let contador = 0;
+
+// while(contador <= numeroDeAlunos) {
+//     //console.log(contador) 
+    
+//     if(contador == 0){
+//         console.log("O numero atual é zero")
+//         }else if(contador % 2 != 0){
+//             console.log("O número " + contador + " é Ímpar")
+//         }else{
+//             console.log(`O número ${contador} é Par`)
+//         }
+//         contador++;
+//     }
+
+// for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+// //console.log(contador) }
+
+// if(contador == 0){
+//     console.log("O numero atual é zero")
+//     }else if(contador % 2 == 0){
+//         console.log("O número " + contador + " é Par")
+//     }else{
+//         console.log(`O número ${contador} é Ímpar`)
+//     }
+// }
